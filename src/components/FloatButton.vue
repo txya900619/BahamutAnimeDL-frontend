b0e79666c5b47609fddedbe0942f1f50bdcfe2fb
wayne900619 <wayne900619@gmail.com>
2020-05-22 19:43:53 +0800

    add queue in floatButton's dialog

--- src/components/FloatButton.vue
+++ src/components/FloatButton.vue
@@ -16,7 +16,27 @@
       </v-btn>
     </v-fab-transition>
     <v-dialog v-model="dialog" max-width="40%">
-      <v-card> </v-card>
+      <v-card>
+        <v-card-title>下載序列</v-card-title>
+        <v-list>
+          <v-list-item v-for="item in queue" :key="item.name + item.ep">
+            <v-list-item-content>
+              <v-list-item-title>{{
+                item.name + " [" + item.ep + "]"
+              }}</v-list-item-title>
+              <v-progress-linear
+                v-if="item.downloading == 1"
+                :buffer-value="100"
+                :value="
+                  (downloadingStatus[item.sequence - 1] /
+                    (item.numberOfChunk + 1)) *
+                    100
+                "
+              ></v-progress-linear>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
     </v-dialog>
   </div>
 </template>
@@ -25,15 +45,29 @@ import { Vue, Component } from "vue-property-decorator";
 @Component
 export default class FloatButton extends Vue {
   dialog = false;
-  queue = [];
+  queue = [
+    {
+      downloading: 0,
+      ep: "0",
+      name: "",
+      numberOfChunk: 1,
+      sequence: 1,
+      sn: 1,
+      spacial: 1,
+      stop: 0,
+    },
+  ];
+  downloadingStatus = [1];
   async mounted() {
     setInterval(() => {
       this.updateQueue();
-    }, 2000);
+    }, 500);
   }
   async updateQueue() {
-    this.queue = await window.getDownloadQueue();
-    console.log(this.queue);
+    const result = await window.getDownloadQueue();
+    console.log(result);
+    this.queue = result.queue;
+    this.downloadingStatus = result.downloading_status;
   }
 }
 </script>
