1980828e0fcfded4700ce01e6c0232ea6bbc98d9
txya900619 <wayne900619@gmail.com>
2020-11-02 00:49:05 +0800

    add icon to search bar and add focus style

--- src/components/App/NavBar.vue
+++ src/components/App/NavBar.vue
@@ -1,12 +1,10 @@
-<!-- @format -->
-
 <template>
   <NavDrawer :value="isScrollLock" @close="isScrollLock = false" />
   <div id="navbar">
     <div class="navbar-item-container menu-toggle" @click="isScrollLock = true">
       <button class="navbar-item">
         <div>
-          <svg style="width: 24px; height: 24px" viewBox="0 0 24 24">
+          <svg viewBox="0 0 24 24">
             <path
               fill="currentColor"
               d="M4,5V7H21V5M4,11H21V9H4M4,19H21V17H4M4,15H21V13H4V15Z"
