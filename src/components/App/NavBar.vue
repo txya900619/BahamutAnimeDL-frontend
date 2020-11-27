dda5d07f2c43b03e11fdc8f1f75540884bf2caf8
txya900619 <wayne900619@gmail.com>
2020-11-27 15:37:25 +0800

    complete navdrawer and search bar base view

--- src/components/App/NavBar.vue
+++ src/components/App/NavBar.vue
@@ -1,18 +1,14 @@
 <template>
   <NavDrawer :value="isScrollLock" @close="isScrollLock = false" />
   <div id="navbar">
-    <div class="navbar-item-container menu-toggle" @click="isScrollLock = true">
-      <button class="navbar-item">
-        <div>
-          <svg viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M4,5V7H21V5M4,11H21V9H4M4,19H21V17H4M4,15H21V13H4V15Z"
-            />
-          </svg>
-        </div>
-      </button>
-    </div>
+    <NavBarButton class="menu-toggle" @click="isScrollLock = true">
+      <svg viewBox="0 0 24 24">
+        <path
+          fill="currentColor"
+          d="M4,5V7H21V5M4,11H21V9H4M4,19H21V17H4M4,15H21V13H4V15Z"
+        />
+      </svg>
+    </NavBarButton>
     <SearchBar />
   </div>
 </template>
@@ -20,10 +16,11 @@
 import { defineComponent, inject, ref } from "vue";
 import NavDrawer from "./NavDrawer.vue";
 import SearchBar from "./SearchBar.vue";
+import NavBarButton from "./NavBarButton.vue";
 import Store from "../../store";
 export default defineComponent({
   name: "NavBar",
-  components: { NavDrawer, SearchBar },
+  components: { NavDrawer, SearchBar, NavBarButton },
   setup() {
     const isScrollLock = inject(Store.isScrollLock, ref(false));
     return {
---
+++ src/components/App/NavBarButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar-item-container navbar-button">
+    <button class="navbar-item">
+      <div>
+        <slot />
+      </div>
+    </button>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "NavBarButton",
+});
+</script>
